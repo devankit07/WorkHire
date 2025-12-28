af4d043d3e2e556d0e828fda9fe1b9aa22ecbbdb
Ankitkumar277 <Ankitrathor272005@gmail.com>
2025-12-28 15:54:26 +0530

    ui update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,22 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <nav className="flex items-center justify-between px-6 py-3">
-      <Link to="/">
-        <img
-          src="/logo.png"
-          alt="WorkHire Logo"
-          className="h-25  w-auto object-contain"
-        />
-      </Link>
-      <Button variant="outline" className="h-10 px-6">
-        Login
-      </Button>
-    </nav>
+    <nav className="flex items-center justify-between px-6 h-25">
+
+  <Link to="/" className="flex items-center h-full">
+    <img
+      src="/logo.png"
+      alt="WorkHire Logo"
+      className="h-40 w-auto object-contain"
+    />
+  </Link>
+
+  
+  <Button variant="outline" className="h-10 px-6">
+    Login
+  </Button>
+</nav>
+
   );
 };
 
